f32bdcce8c34081e23c3323bb983dc1e6019681a
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-09-01 22:03:33 +0300

    clean app and create app colors

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff0072D2);
+  static const Color blueDark = Color(0xff1A1C28);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff000000);
+}
