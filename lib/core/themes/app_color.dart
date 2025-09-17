6bf7af14ff12e367eef019d146aafcad46dadbdf
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-09-17 20:11:29 +0300

    update readme file

--- lib/core/themes/app_color.dart
+++ lib/core/themes/app_color.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const Color primary = Color(0xff0072D2);
+  static const Color blueLight = Color(0xFF1198C5);
   static const Color blueDark = Color(0xff1A1C28);
   static const Color black = Color(0xff000000);
   static const Color grey = Color(0xff898989);
